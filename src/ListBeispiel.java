3d526c3f454f8d1ca5127e726b856a2d7b21efe9
Philipp Cedrych <philipp.cedrych@compax.at>
2023-11-09 16:11:31 +0100

    Implementation

--- src/ListBeispiel.java
+++ src/ListBeispiel.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ListBeispiel {
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListBeispiel {
+
+  public static void main(String[] args) {
+    List<Object> list = new ArrayList<>();
+
+    list.add("Hallo");
+    list.add(42);
+    list.add(3.14);
+    list.add("Welt");
+    list.add(42); // Duplikat wird hinzugefügt
+
+    System.out.println("Inhalt der Liste:");
+    for (Object obj : list) {
+      System.out.println(obj);
+    }
+  }
 }
--- src/SetBeispiel.java
+++ src/SetBeispiel.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class SetBeispiel {
+import java.util.HashSet;
+import java.util.Set;
+
+public class SetBeispiel {
+
+  public static void main(String[] args) {
+    Set<Object> set = new HashSet<>();
+
+    set.add("Hallo");
+    set.add(42);
+    set.add(3.14);
+    set.add("Welt");
+    set.add(42); // Duplikat wird nicht hinzugefügt
+
+    System.out.println("Inhalt des Sets:");
+    for (Object obj : set) {
+      System.out.println(obj);
+    }
+  }
 }
